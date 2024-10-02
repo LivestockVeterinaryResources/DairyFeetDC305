f1af84c041465b84443fc2737527eab88bb2ca21
Gerard Cramer <gerard@dairycowfootdoc.com>
2024-10-02 16:58:41 -0500

    created functions for KM graphs and tables

--- 15_noninf_survival_multiple_farms_function.R
+++ 15_noninf_survival_multiple_farms_function.R
@@ -1,21 +1,20 @@
 # Survival post lesion Dx
 
-#Survival:Blocks----
+#Survival after lesion dx:----
 
-## function
-
-survival_data <- function(data = lamecull, censor_var = censordat,
+## function to create data set for further use
+survival_data <- function(data = lamecull, censor_days = censordat,
                           disease_date = ftdat, culled = culled,
                           control = lifexlame, life_x_disease) {
   # need to do this to use as.numeric to convert duration
-  #censor_date <- ensym(censor_var)
+  #censor_days <- ensym(censor_var)
   data |> 
     # reduce data
-    select(farm, cowid, {{disease_date}}, {{censor_var}}, {{culled}},
+    select(farm, cowid, {{disease_date}}, {{censor_days}}, {{culled}},
            {{control}}, {{life_x_disease}}
            ) |>  
     ## this as.numeric creates NA's not sure why as it works without function
-  mutate (censor_time = as.numeric({{censor_var}}),
+  mutate (censor_time = as.numeric({{censor_days}}),
          # create variables to condition on
          life_x_disease = case_when({{ control }} == 0 ~ 0,
                                     {{ life_x_disease }} == 1 ~ 1,
@@ -36,106 +35,82 @@ survival_data <- function(data = lamecull, censor_var = censordat,
          )
 }
 
-# create dataset
+# km function to create graph
+km_fit <- function(data, time = censor_time, event = culled) {
+  data_surv <- data |> 
+    mutate(surv_object = Surv(time = censor_time, event = culled))
+  
+  fit_km <- survfit(surv_object ~ life_x_disease_cat, data = data_surv)
+  fit_km |> 
+    ggsurvplot(
+      # needed data statement as extracts see help and without it doesn't work
+      data = data_surv, 
+      pval = FALSE, 
+      conf.int = TRUE,
+      censor = FALSE,
+      fun = "pct",
+      size = 1,
+      linetype = c(1, 2, 3, 4), # Specify dashed lines for each group
+      palette = c("#fde725ff", "#440154ff", "#3b528bff",
+                  "#21908cff"), 
+      legend = "bottom",
+      legend.title = "Lifetime Lesion #",
+      legend.labs = c("Never any lesion",
+                      "First",
+                      "Second",
+                      "3 or more"),
+      xlab = "Days to cull after trimming/diagnosis",
+      ylab = "% of Cows Alive",
+      xlim = c(0,180),
+      ylim = c(50,100),
+      break.time.by = 30
+    )
+}
+
+# function for table
+km_fit_table <- function(data, time = censor_time, event = culled) {
+  data_surv <-data |> 
+    mutate(surv_object = Surv(time = censor_time, event = culled))
+  
+  fit_table <- survfit(surv_object ~ life_x_disease_cat, data = data_surv)
+  fit_table |> 
+    tbl_survfit(times = c(30,120, 180),
+                label = " ",
+                reverse = TRUE, 
+                label_header = "{time} Days") %>% 
+    as_flex_table() %>% # makes formatting and pdf better
+    add_header_row(top = TRUE,
+                   values = c("Lifetime Lesion History",
+                              "% Culled (Confidence Interval) at",
+                              "", "")) %>% 
+    bold(i = 1, bold = TRUE, part = "header") %>% # bolds headers
+    bold(i = 2, bold = TRUE, part = "header") %>% 
+    merge_at(i = 1:2, j = 1, part = "header") %>% #merges 1st row
+    merge_at(i = 1, j = 2:4, part = "header") %>% # merges top columns
+    delete_rows(i = 1, part = "body")|>
+    fit_to_width(max_width= 6.5) |> 
+    hline_bottom(part = "body")
+}
+
+# commands to add into QMD----
+# create single dataset
 surval_inf <- survival_data(life_x_disease = lifexnoninf, 
                             disease_date = ftdat)
 
-# fit KM data 
-# not working yet
-km_fit <- function(data, time = censor_time, event = culled) {
-  data_surv <- data |> 
-    mutate(test = 1)
-}
+# create graph
+km_graph <- km_fit(data = surval_inf, event = culled)
 
-# this name of surv_object is funky
-km_fit <- function(data, time = censor_time, event = culled) {
-  data_surv <- data |> 
-    mutate(surv_object = Surv(time = {{time}}, event = {{event}})
-    ) 
-  }
-  
-# attempt to fix col names
-km_fit <- function(data, time = censor_time, event = culled) {
-  data_surv <- data |> 
-    mutate(surv_time = Surv(time = {{time}}),
-           surv_event = Surv({{event}})
-    ) 
-}
+# km table
+km_table <- km_fit_table(data = surval_inf, event = culled)
 
-## still doesn't work
-km_fit <- function(data, time = censor_time, event = culled) {
-  data_surv <- data 
-    fit <- survfit(Surv(time = {{ time }}, 
-                        event = {{ event }}) ~
-                    life_x_disease, data = data_surv)
-  }
-  
-# chatgpt
-library(survival)
-
-km_fit <- function(data, time, event) {
-  data_surv <- data 
-  fit <- survfit(Surv(time = !! time, event = !! event) ~ life_x_disease, data = data_surv)
-}
-
-# Example usage:
-# km_fit(data, time = data$censor_time, event = data$culled)
+# or combo approach
+km_graph2 <- survival_data(life_x_disease = lifexinf, disease_date = ftdat) |> 
+  km_fit(event = culled)
 
 
-censor_days = enquo(censor_time)
-censor_var = enquo(event)
-km_test <- km_fit(data = surval_inf)
-km_test <- km_fit(data = surval_inf, 
-                  time = censor_time, event = culled)
-
-fitKM <- survfit(Surv(censordat, culled) ~ lifexnoninf, data = survall_1y)
-km<- ggsurvplot(fitKM, 
-                pval = FALSE, 
-                conf.int = TRUE,
-                censor = FALSE,
-                fun = "pct",
-                size = 1,
-                linetype = c(1, 2, 3, 4), # Specify dashed lines for each group
-                palette = c("#fde725ff", "#440154ff", "#3b528bff",
-                            "#21908cff"), 
-                legend = "bottom",
-                legend.title = "Lifetime Lesion #",
-                legend.labs = c("Never any lesion",
-                                "First",
-                                "Second",
-                                "3 or more"),
-                #   "Combined"), for add.all
-                xlab = "Days to cull after lifetime lesion #",
-                ylab = "% of Cows Alive",
-                xlim = c(0,180),
-                ylim = c(50,100),
-                #add.all = TRUE,
-                tables.theme = theme_cleantable(),
-                break.time.by = 30
-                ) 
-
-# tables for survival
-
-fit_farm1 <- survfit(Surv(censordat, culled) ~ lifexnoninf_cat, 
-                     data = survall_1y)
+# todo: create list of all datasets for lesions 
+# then feed to graph function
 
 
-tbl_surv_farm1 <- tbl_survfit(fit_farm1, 
-                              times = c(30,120, 180),
-                              label = " ",
-                              reverse = TRUE, 
-                              label_header = "{time} Days") %>% 
-  as_flex_table() %>% # makes formatting and pdf better
-  add_header_row(top = TRUE,
-                 values = c("Lifetime Lesion History",
-                            "% Culled (Confidence Interval) at",
-                            "", "")) %>% 
-  bold(i = 1, bold = TRUE, part = "header") %>% # bolds headers
-  bold(i = 2, bold = TRUE, part = "header") %>% 
-  merge_at(i = 1:2, j = 1, part = "header") %>% #merges 1st row
-  merge_at(i = 1, j = 2:4, part = "header") %>% # merges top columns
-  delete_rows(i = 1, part = "body")|>
-  fit_to_width(max_width= 6.5) |> 
-  hline_bottom(part = "body")
 
 
